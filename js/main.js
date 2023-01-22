14aa91c9127cd248c2cc112e5d68261ba4e7820a
Barbod Garsha <barbodgarshasbi79@gmail.com>
2023-01-22 19:44:25 +0100

    the sub menus are now functional

--- js/main.js
+++ js/main.js
@@ -4,10 +4,18 @@ shadow = document.querySelector('[data-shadow]')
 header_nav = document.querySelector('[data-header-nav]')
 header_nav_close_menu_btn = document.querySelector('[data-header-close-menu]')
 header_nav_open_menu_btn = document.querySelector('[data-header-open-menu]')
- 
+
+header_nav_items__has_sub = document.querySelectorAll('[data-header-nav-item--has-sub]')
+
+header_nav_items__has_sub.forEach(el => {
+    el.addEventListener('click', function(e) {
+        e.target.classList.toggle('header__nav__menu__item--sub-closed')
+    })
+})
+
 pre_load_hidden.forEach(el => {
     el.classList.remove('pre-load--hide')
-});
+})
 
 
 header_nav_open_menu_btn.addEventListener('click', function(e) {
